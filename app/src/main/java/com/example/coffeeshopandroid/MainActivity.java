ff23d09e25fb74643353f03f9908aaaf082d0a21
Prabhjeetkaur515 <84962197+Prabhjeetkaur515@users.noreply.github.com>
2022-05-11 09:40:31 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/coffeeshopandroid/MainActivity.java
+++ app/src/main/java/com/example/coffeeshopandroid/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Signup.class);
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
                 startActivity(intent);
             }
         });
